cc320f43007614d5cca98c13d990ec228d3273ce
Leon <12817042+thedevleon@users.noreply.github.com>
2025-03-25 18:30:12 +0100

    wip: add esp32c6 example

---
+++ examples/esp32c6/src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use esp_hal::clock::CpuClock;
+use esp_hal::timer::systimer::SystemTimer;
+use panic_rtt_target as _;
+
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    rtt_target::rtt_init_defmt!();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timer0 = SystemTimer::new(peripherals.SYSTIMER);
+    esp_hal_embassy::init(timer0.alarm0);
+
+    info!("Embassy initialized!");
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+}
